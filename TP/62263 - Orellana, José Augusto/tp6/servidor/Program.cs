60ea7f40c59dda01922f29a8a32b1eb0beedc9e5
Jao <109613248+Jaodem@users.noreply.github.com>
2025-06-10 11:57:45 -0300

    creacion de clases

--- TP/62263 - Orellana, José Augusto/tp6/servidor/Program.cs
+++ TP/62263 - Orellana, José Augusto/tp6/servidor/Program.cs
@@ -29,3 +29,55 @@ app.MapGet("/", () => "Servidor API está en funcionamiento");
 app.MapGet("/api/datos", () => new { Mensaje = "Datos desde el servidor", Fecha = DateTime.Now });
 
 app.Run();
+
+class Producto
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = null!;
+    public string Descripcion { get; set; } = null!;
+    public decimal Precio { get; set; }
+    public int Stock { get; set; }
+    public string ImageUrl { get; set; } = null!;
+
+    public List<ItemCompra> ItmsCompra { get; set; } = new(); // Relación uno a muchos
+}
+
+class Compra
+{
+    public int Id { get; set; }
+    public DateTime Fecha { get; set; }
+    public decimal Total { get; set; }
+
+    public string NombreCliente { get; set; } = null!;
+    public string ApellidoCliente { get; set; } = null!;
+    public string EmailCliente { get; set; } = null!;
+
+    public List<ItemCompra> Items { get; set; } = new(); // Relación uno a muchos
+}
+
+class ItemCompra
+{
+    public int Id { get; set; }
+
+    public int ProductoId { get; set; }
+    public Producto producto { get; set; } = null!;
+
+    public int CompraId { get; set; }
+    public Compra compra { get; set; } = null!;
+
+    public int Cantidad { get; set; }
+    public decimal PrecioUnitario { get; set; }
+}
+
+class TiendaContext : DbContext
+{
+    public TiendaContext(DbContextOptions<TiendaContext> options) : base(options) { }
+
+    public DbSet<Producto> Productos => Set<Producto>();
+    public DbSet<Compra> Compras => Set<Compra>();
+    public DbSet<ItemCompra> ItemsCompra => Set<ItemCompra>();
+}
+
+record ClienteDTO(string Nombre, string Apellido, string Email);
+
+record ItemCantidadDTO(int Cantidad);
