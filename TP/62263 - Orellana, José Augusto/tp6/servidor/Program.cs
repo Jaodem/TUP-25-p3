98ede56650f9b0afe8a54a4d722e4ecc10f7947c
Jao <109613248+Jaodem@users.noreply.github.com>
2025-06-10 21:27:20 -0300

    endpoint para confirmar carrito de compras

--- TP/62263 - Orellana, José Augusto/tp6/servidor/Program.cs
+++ TP/62263 - Orellana, José Augusto/tp6/servidor/Program.cs
@@ -98,6 +98,69 @@ app.MapDelete("/carritos/{carritoId:int}", async (int carritoId, TiendaContext d
     return Results.Ok("Carrito vaciado exitosamente.");
 });
 
+// Endpoint para confirmar un carrito de compras
+app.MapPut("/carritos{carritoId:int}/confirmar", async (int carritoId, ClienteDTO cliente, TiendaContext db) =>
+{
+    // Validar campos obligatorios
+    if (string.IsNullOrWhiteSpace(cliente.Nombre) ||
+        string.IsNullOrWhiteSpace(cliente.Apellido) ||
+        string.IsNullOrWhiteSpace(cliente.Email))
+    {
+        return Results.BadRequest("Todos los campos del cliente son obligatorios.");
+    }
+
+    // Validar formato de email
+    if (!cliente.Email.Contains("@")) return Results.BadRequest("El email debe ser válido, es decir, contener @");
+
+    var compra = await db.Compras
+        .Include(c => c.Items)
+        .ThenInclude(ic => ic.Producto)
+        .FirstOrDefaultAsync(c => c.Id == carritoId);
+
+    if (compra == null || !compra.Items.Any()) return Results.BadRequest("Carrito no encontrado o vacío");
+
+    // Validación de carrito ya confirmado
+    if (!string.IsNullOrWhiteSpace(compra.NombreCliente) &&
+        !string.IsNullOrWhiteSpace(compra.ApellidoCliente) &&
+        !string.IsNullOrWhiteSpace(compra.EmailCliente) &&
+        compra.Total > 0)
+    {
+        return Results.BadRequest("Este carrito ya fue confirmado anteriormente");
+    }
+
+    foreach (ver item in compra.Items)
+    {
+        if (item.Cantidad > item.Producto.Stock) return Results.BadRequest($"No hay stock suficiente para el producto {item.Producto.Nombre}");
+    }
+
+    // Actualizar stock de productos y calcular total
+    decimal total = 0;
+    foreach (var item in compra.Items)
+    {
+        item.Producto.Stock -= item.Cantidad;
+        total += item.Cantidad * item.PrecioUnitario;
+    }
+
+    // Actualizar datos del cliente
+    compra.NombreCliente = cliente.Nombre;
+    compra.ApellidoCliente = cliente.Apellido;
+    compra.EmailCliente = cliente.Email;
+    compra.Fecha = DateTime.Now;
+    compra.Total = total;
+
+    await db.SaveChangesAsync();
+
+    return Results.Ok(new
+    {
+        compra.Id,
+        compra.Total,
+        Fecha = compra.Fecha.ToString("dd/MM/yyyy HH:mm"),
+        Cliente = new { compra.NombreCliente, compra.ApellidoCliente, compra.EmailCliente }
+    });
+});
+
+// Endpoint para agregar un producto al carrito (o actualizar la cantidad)
+
 app.Run();
 
 class Producto
